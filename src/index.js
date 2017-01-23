58a1f4432e3ac1657c37da5902edd692139aa654
Enrique Burillo <enrique.burillo@gluckgames.com>
2017-01-24 00:12:07 +0100

    Introducing react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App}></Route>
+  </Router>
+), document.getElementById('root'));
